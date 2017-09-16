03bb5b257931ac90778ee107a8602bc571b4dbd1
Julian Chan <julian@Julians-MacBook-Pro.local>
2017-09-16 20:07:00 +1000

    added mocha test - one equals one test

---
+++ tests/spec.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+describe('One', function() {
+   describe('#equalsOne()', function() {
+        it('should be equal to one', function(done) {
+            assert.equal(1, 1);
+            done();
+        });
+    });
+});

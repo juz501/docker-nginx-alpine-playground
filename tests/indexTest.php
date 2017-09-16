56f018f6517b8d75e4df3617191541b38f3a72d5
Julian Chan <julian@Julians-MacBook-Pro.local>
2017-09-16 23:37:19 +1000

    added dummy test

---
+++ tests/indexTest.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Index
+ */
+ final class IndexTest extends TestCase
+ {
+    public function testRandom() : void
+    {
+        $this->assertEquals(true, true);
+    }
+
+ }
